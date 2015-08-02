4fec9cd79bccb73a672f263ff2e376d7f901546f
beto-rodriguez <rodriguezo.alberto@gmail.com>
2015-08-02 18:17:21 -0500

    Initial

---
+++ SuperXML/Compiler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace SuperXml
+{
+    public class Compiler
+    {
+        public Dictionary<string, dynamic> Scope { get; set; }
+        public XmlNode Node { get; set; }
+
+        public Compiler SetDocument(XmlNode xmlNode)
+        {
+            Node = xmlNode;
+            return this;
+        }
+
+        public Compiler SetScope(Dictionary<string, dynamic> scope)
+        {
+            Scope = scope;
+            return this;
+        }
+
+        public Compiler AddElementToScope(string key, dynamic value)
+        {
+            if (Scope == null) Scope = new Dictionary<string, dynamic>();
+            Scope.Add(key, value);
+            return this;
+        }
+
+        public XmlNode Compile()
+        {
+            Scope = Scope ?? new Dictionary<string, dynamic>();
+            if (Node == null) return null;
+            return Node.Compile(Scope);
+        }
+    }
+}
---
+++ SuperXML/Extensions.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using NCalc;
+using Newtonsoft.Json;
+
+namespace SuperXml
+{
+    public static class Extensions
+    {
+        static Extensions()
+        {
+            IsExpressionRegex = new Regex("(?<={{).*?(?=}})");
+            ForEachRegex = 
+                new Regex(@"^\s*([a-zA-Z_]+[\w]*)\s+in\s+(([a-zA-Z][\w]*(\.[a-zA-Z][\w]*)*)|\[(.+)(,\s*.+)*\])\s*$");
+            IsXmlTagClosedRegex = new Regex(@"^<.*\/\s*>.*$", RegexOptions.Singleline);
+        }
+
+        private static readonly Regex IsExpressionRegex;
+        private static readonly Regex ForEachRegex;
+        private static readonly Regex IsXmlTagClosedRegex;
+
+        public static XmlNode Compile(this XmlNode node, Dictionary<string, dynamic> s)
+        {
+            var content = node.CompileString(s);
+            var doc = new XmlDocument();
+            doc.LoadXml(content);
+            return doc.DocumentElement;
+        }
+
+        private static string CompileString(this XmlNode node, Dictionary<string, dynamic> s)
+        {
+            if (!node.If(s)) return "";
+
+            var xml = "";
+            var nodeMarkUp = "";
+
+            foreach (var expansion in node.ForEach(s))
+            {
+                if (node.NodeType == XmlNodeType.Element)
+                {
+                    var isExpression = node.Name == "Expression";
+
+                    xml = !isExpression
+                        ? node.OuterXml.Substring(0, node.OuterXml.IndexOf(">", StringComparison.Ordinal) + 1)
+                        : "";
+                    nodeMarkUp = xml;
+
+                    if (!isExpression)
+                        foreach (Match e in IsExpressionRegex.Matches(xml))
+                            xml = xml.Replace("{{" + e.Value + "}}",
+                                EvaluateExpression(e.Value, expansion.Scope));
+
+                    xml = node.ChildNodes.Cast<XmlNode>()
+                        .Aggregate(xml, (current, child) => current + child.CompileString(expansion.Scope));
+                }
+                else
+                {
+                    foreach (Match e in IsExpressionRegex.Matches(node.InnerText))
+                    {
+                        xml = node.InnerText.Replace("{{" + e.Value + "}}", EvaluateExpression(e.Value, expansion.Scope));
+                    }   
+                }
+            }
+            return xml + (IsXmlTagClosedRegex.IsMatch(nodeMarkUp) ? "" : "</" + node.Name + ">");
+        }
+
+        private static IEnumerable<ExpandedNode> ForEach(this XmlNode node, Dictionary<string, dynamic> s)
+        {
+            if (node == null) yield break;
+            var exp = node.ReadAttrAsString("ForEach");
+            if (exp == null)
+            {
+                yield return new ExpandedNode { Node = node, Scope = s };
+                yield break;
+            }
+
+            exp = exp.Replace(Environment.NewLine, "");
+                
+            if (!ForEachRegex.IsMatch(exp))
+                throw new Exception(
+                    "Compilation Error: ForEach was expecting an expression like " +
+                    "'varName in [value1, value2, value3..., valueN]'");
+
+            var match = ForEachRegex.Match(exp);
+            var repeater = match.Groups[1].ToString();
+            var scopeName = match.Groups[3].ToString();
+            var scopeRoot = scopeName.Split('.')[0];
+            var items = scopeName.Length > 0
+                ? (s.ContainsKey(scopeRoot) ? NavigateTo(s[scopeRoot], scopeName) : new int[] { })
+                : JsonConvert.DeserializeObject<dynamic>(match.Groups[2].ToString());
+
+            var cs = s.ToDictionary(x => x.Key, x => x.Value);
+
+            Action<string, dynamic> addToScope = (key, value) =>
+            {
+                if (cs.ContainsKey(key)) cs[key] = value;
+                else cs.Add(key, value);
+            };
+
+            var i = 0;
+            foreach (var item in items)
+            {
+                addToScope(repeater, item);
+                addToScope("$index", i++);
+                yield return new ExpandedNode { Node = node, Scope = cs };
+            }
+        }
+
+        private static bool If(this XmlNode node, Dictionary<string, dynamic> s)
+        {
+            if (node == null) return false;
+            var exp = node.ReadAttr("If");
+            if (exp == null) return true;
+
+            var eval = EvaluateExpression(exp, s);
+
+            bool res;
+            var couldConvert = bool.TryParse(eval, out res);
+
+            return couldConvert && res;
+        }
+
+        public static string EvaluateExpression(string expression, Dictionary<string, dynamic> scope)
+        {
+            var originalExpression = expression;
+#if(DEBUG)
+            var starti = DateTime.Now;
+#endif
+            var p = 0;
+            var parameters = new Dictionary<string, object>();
+            var regex = new Regex(@"[\s|&=!<>+\-*/%^(]([A-Za-z_$]\w*(\.[A-Za-z_][\w()]*)*)");
+
+            foreach (Match match in regex.Matches(" " + expression))
+            {
+                var g = match.Groups[1].Value;
+                var varName = g.Split('.')[0].Trim();
+                dynamic varValue = scope.ContainsKey(varName)
+                    ? NavigateTo(scope[varName], g)
+                    : "";
+                expression = expression.Replace(g, "[p" + p + "]");
+                parameters.Add("p" + p, varValue);
+                p++;
+            }
+#if(DEBUG)
+            Trace.WriteLine("Injection time " + (DateTime.Now - starti).TotalMilliseconds + "ms");
+            var starte = DateTime.Now;
+#endif
+            if (string.IsNullOrWhiteSpace(expression)) return "";
+            var e = new Expression(expression.Replace("&gt;", ">").Replace("&lt;", "<"));
+            foreach (var parameter in parameters) e.Parameters[parameter.Key] = parameter.Value;
+
+            try
+            {
+                var result = e.Evaluate().ToString();
+#if(DEBUG)
+                Trace.WriteLine("Evaluation time: " + (DateTime.Now - starte).TotalMilliseconds + "ms");
+#endif
+                return result;
+            }
+            catch
+            {
+#if(DEBUG)
+                Trace.WriteLine("Evaluation time with exception thrown: " + (DateTime.Now - starte).TotalMilliseconds + "ms");
+#endif
+                return originalExpression;
+            }
+        }
+
+        private static dynamic NavigateTo(dynamic obj, string propertyName)
+        {
+            var name = propertyName.Split('.').ToList();
+            if (name.Count == 1) return obj;
+
+            var level = 1;
+            do
+            {
+                obj = obj.GetType().GetProperty(name[level]).GetValue(obj, null);
+                level++;
+            } while (level < name.Count);
+
+            return obj;
+        }
+
+        private class ExpandedNode
+        {
+            public XmlNode Node { get; set; }
+            public Dictionary<string, dynamic> Scope { get; set; }
+        }
+
+        private static string ReadAttr(this XmlNode node, string attribute, string defaultValue = null)
+        {
+            if (attribute == null || node.Attributes == null)
+                return null;
+
+            return node.Attributes[attribute] == null ? defaultValue : node.Attributes[attribute].Value;
+        }
+
+        public static string ReadAttrAsString(this XmlNode node, string attribute, string defaultValue = null)
+        {
+            if (attribute == null || node.Attributes == null)
+                return null;
+
+            return node.Attributes[attribute] == null ? defaultValue : node.Attributes[attribute].Value;
+        }
+
+    }
+}
