58052a577ff3e870c590e24fabd7274f881cee8d
beto-rodriguez <rodriguezo.alberto@gmail.com>
2015-08-25 11:30:32 -0500

    3.0.0

--- SuperXML/Properties/AssemblyInfo.cs
+++ SuperXML/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Templator")]
+[assembly: AssemblyTitle("SuperXml")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Controli")]
-[assembly: AssemblyProduct("Templator")]
+[assembly: AssemblyProduct("SuperXml")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5e685140-a614-4fea-8097-d010046f5f5a")]
-[assembly: AssemblyVersion("1.0.9")]
-[assembly: AssemblyFileVersion("1.0.9")]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
